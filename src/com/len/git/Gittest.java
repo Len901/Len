64ff5e88717d757ba1db4ab43eca2859d5eeed49
Len <1356342738@qq.com>
2022-12-22 23:47:17 +0800

    first commit

---
+++ src/com/len/git/Gittest.java
@@ -0,0 +1,11 @@
+package com.len.git;
+
+/**
+ * @auther Len901
+ * @time 2022-12-22-23:41
+ */
+public class Gittest {
+    public static void main(String[] args) {
+        System.out.println("hello，git!");
+    }
+}
