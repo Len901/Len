5be4f387a5b2cb89e762282b74750e3a31f2505f
Len <1356342738@qq.com>
2022-12-23 08:04:29 +0800

    hot-fix first commit

--- src/com/len/git/Gittest.java
+++ src/com/len/git/Gittest.java
@@ -9,5 +9,6 @@ public class Gittest {
         System.out.println("hello，git!");
         System.out.println("hello,Len");
         System.out.println("hello,world");
+        System.out.println("hello，wrx");
     }
 }
